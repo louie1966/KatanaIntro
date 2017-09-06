c4830dadb87e24cbed5ad6e549b258303c9671c2
louie1966 <renedeleeuw66@gmail.com>
2017-09-06 13:34:42 +0200

    WebApp Working but run disabled. Upnext => https://app.pluralsight.com/player?course=aspdotnet-mvc5-fundamentals&author=scott-allen&name=aspdotnet-mvc5-fundamentals-m2-katana&clip=4&mode=live

--- KatanaIntro/Program.cs
+++ KatanaIntro/Program.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Owin.Hosting;
+using System;
 
 namespace KatanaIntro {
     class Program {
         static void Main(string[] args) {
+            string uri = "http://localhost:8080";
+
+            using (WebApp.Start<Startup>(uri)) {
+                Console.WriteLine("Started!");
+                Console.ReadKey();
+                Console.WriteLine("Stopped");
+            }
         }
     }
 }
